957b287fb97f7f6119e01db39267b6f668ace213
viveksh81681 <45921293+viveksh81681@users.noreply.github.com>
2022-01-21 21:32:31 +0530

    Main
    Complete Clone of LYST website

---
+++ scripts/footer.js
@@ -0,0 +1,54 @@
+function footer(){
+    return `<div id = "mainhead">
+    <hr>
+    <div id="maindiv">
+        <div id="onediv">
+            <div id="inner">
+                <img id="getiton" src="/images/app-store-google-play-logo-4A2747BF5E-seeklogo.png" alt="">
+            </div>
+        </div>
+        <div id="twodiv">
+            <h2 id="footerheading">WOMEN</h2>
+            <p>Ankle boots</p>
+            <p>Coctail dresses</p>
+            <p>Gowns</p>
+            <p>Leather Jackets</p>
+            <p>Maxi and long dresses</p>
+            <p>Skinny Jeans</p>
+            <p>Sweaters</p>
+            <p>Pullovers</p>
+        </div>
+        <div id="threediv">
+            <h2 id="footerheading">MEN</h2>
+            <p>Leather Jackets</p>
+            <p>Oxfords</p>
+            <p>Polo Shirts</p>
+            <p>Raincoats and Trench</p>
+            <p>Coats</p>
+            <p>Loafers</p>
+            <p>High Top Sneakers</p>
+            <p>Hoodies</p>
+        </div>
+        <div id="fourdiv">
+            <h2 id="footerheading">HELP AND INFO</h2>
+            <P>Help Center</P>
+            <P>About Us</P>
+            <P>Shipping Policy</P>
+            <P>Returns Policy</P>
+            <P>Payments</P>
+            <P>Refund Policy</P>
+            <P>Developers</P>
+            <P>Careers</P>
+            <P>Contact</P>
+            <P>Terms & Conditions</P>
+            <P>Privacy & Cookie Policy</P>
+            <P>Intellectual Property</P>
+            <P>Categories</P>
+            <P>Become a Partner</P>
+            <P>Lyst Insights</P>
+        </div>
+    </div>
+</div>`
+}
+
+export default footer
---
+++ scripts/navbar.js
@@ -0,0 +1,20 @@
+function navbar(){
+    return `<div id="main">
+    <div id="innersmall">  
+        <button id="join"><a id="signup1" href="signup.html">Join</a></button>
+        <h3 id="points"><a href="signin.html">Sign In</a></h3>
+        <h3 id="points">Help</h3>
+        <h3 id="points">US-USS</h3>
+    </div>
+    <div id="innerbig">
+        <h1 id="banner"><a href="home.html">LYST</a></h1>
+        <h2 id="part"><a href="activewear.html">WOMEN</a></h2>
+        <h2 id="part"><a href="mens.html">MEN</a></h2>
+        <input id="inputbox" type="text" placeholder="SEARCH (E.G.VALENTINO DRESSES)">
+        <h2 id="part1"><a href="cart.html">CART</a></h2>
+    </div>
+    <hr>
+</div>`
+};
+
+export default navbar
